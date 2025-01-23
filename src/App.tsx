854b0444e0083b8e88794afae6e2b73c15f50f30
IgorSv <igorexa08@gmail.com>
2025-01-23 16:43:25 +0100

    lesson06_TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import "./App.css";
+import './App.css'
+
+//lessons
+import Lesson06 from './lessons/Lesson06/Lesson06'
+
+//homeworks
+
+//consultations
 
 function App() {
-  return <div className="app">Hello TS</div>;
+
+  return (
+    <div className='app'>
+      {/* Topic: TypeScript - Introduction */}
+      <Lesson06/>
+    </div>
+  )
 }
 
-export default App;
+export default App
