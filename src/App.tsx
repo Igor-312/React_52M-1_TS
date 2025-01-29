ed05683b2cd38696a4bdd1fa95a087fac88da1d1
IgorSv <igorexa08@gmail.com>
2025-01-29 10:20:02 +0100

    homework09

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import GlobalStyles from "./styles/GlobalStyles";
 // import Lesson06 from './lessons/Lesson06/Lesson06'
 // import Lesson07 from './lessons/Lesson07/Lesson07'
 // import Lesson08 from './lessons/Lesson08/Lesson08'
-import Lesson09 from "./lessons/Lesson09/Lesson09";
+// import Lesson09 from "./lessons/Lesson09/Lesson09";
 
 //homeworks
 // import Homework08 from './homeworks/Homework08/Homework08'
+import Homework09 from "./homeworks/Homewrok09/Homewrok09";
 
 //consultations
 // import Consultation03 from './consultations/Consultation03/Consultation03'
@@ -25,7 +26,8 @@ function App() {
       {/* <Lesson08 /> */}
       {/* <Homework08 /> */}
       {/* Topic: Control components, useEffect */}
-      <Lesson09 />
+      {/* <Lesson09 /> */}
+      <Homework09/>
     </>
   );
 }
