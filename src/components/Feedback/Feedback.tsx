24c5acf6c01b42d9fdf4c4928bedd60b1f84e915
IgorSv <igorexa08@gmail.com>
2025-02-02 19:03:55 +0100

    Lesson12_Formik_Yup

--- src/components/Feedback/Feedback.tsx
+++ src/components/Feedback/Feedback.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Button from "../Button/Button";
+import Button from "components/Button/Button";
 import { FeedbackContainer, FeedbackResultContainer, LikeDislikeContainer, Result } from "./styles";
 
 function Feedback() {
