26c8a3f097f071bdac721e764912d6c45f856d58
IgorSv <igorexa08@gmail.com>
2025-02-04 22:30:27 +0100

    Homework13

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,10 @@ import User from 'pages/Users/components/User/User'
 // import Homework09 from './homeworks/Homework09/Homework09'
 // import Homework10 from './homeworks/Homework10/Homework10'
 // import Homework12 from 'homeworks/Homework12/Homework12'
+import Clients from 'pages/Clients/Clients'
+import Facebook from 'pages/Clients/components/Facebook/Facebook'
+import Google from 'pages/Clients/components/Google/Google'
+import Amazon from 'pages/Clients/components/Amazon/Amazon'
 
 //consultations
 // import Consultation03 from './consultations/Consultation03/Consultation03'
@@ -44,6 +48,12 @@ function App() {
           <Route path='/about' element={<About />} />
           <Route path='/users' element={<Users />} />
           <Route path='/users/user' element={<User />} />
+          <Route path='/clients' element={<Clients/>}/>
+          <Route path='/clients/facebook' element={<Facebook/>}/>
+          <Route path='/clients/google' element={<Google/>}/>
+          <Route path='/clients/amazon' element={<Amazon/>}/>
+          {/* Route * - этот route показывает контент, если маршрута не существует в перечисленных выше */}
+          <Route path='*' element='Page not found'/>
         </Routes>
       </Layout>
       {/* --Topic: TypeScript - Introduction */}
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import {
   LayoutComponent,
   Header,
@@ -5,45 +7,56 @@ import {
   LogoContainer,
   Main,
   Footer,
-  StyledNavLink
-} from './styles'
-import { LayoutProps } from './types'
+  StyledNavLink,
+} from "./styles";
+import { LayoutProps } from "./types";
 
 function Layout({ children }: LayoutProps) {
   return (
     <LayoutComponent>
       <Header>
-        <LogoContainer></LogoContainer>
+        <Link to='/'><LogoContainer></LogoContainer></Link>
         <NavContainer>
           <StyledNavLink
-            to='/'
-            style={
-              ({ isActive }) => ({ textDecoration: isActive ? 'underline' : 'none' })
-            }>
+            to="/"
+            style={({ isActive }) => ({
+              textDecoration: isActive ? "underline" : "none",
+            })}
+          >
             Home
           </StyledNavLink>
           <StyledNavLink
-            to='/about'
-            style={
-              ({ isActive }) => ({ textDecoration: isActive ? 'underline' : 'none' })
-            }>
+            to="/about"
+            style={({ isActive }) => ({
+              textDecoration: isActive ? "underline" : "none",
+            })}
+          >
             About
           </StyledNavLink>
           <StyledNavLink
-            to='/users'
-            style={
-              ({ isActive }) => ({ textDecoration: isActive ? 'underline' : 'none' })
-            }>
+            to="/users"
+            style={({ isActive }) => ({
+              textDecoration: isActive ? "underline" : "none",
+            })}
+          >
             Users
+          </StyledNavLink>
+          <StyledNavLink
+            to="/clients"
+            style={({ isActive }) => ({
+              textDecoration: isActive ? "underline" : "none",
+            })}
+          >
+            Clients
           </StyledNavLink>
         </NavContainer>
       </Header>
       <Main>{children}</Main>
       <Footer>
-        <LogoContainer></LogoContainer>
+      <Link to='/'><LogoContainer></LogoContainer></Link>
       </Footer>
     </LayoutComponent>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
